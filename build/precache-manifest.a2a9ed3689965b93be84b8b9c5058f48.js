811e958c0bc5cfc48fc26af0bc4d28193e6a24b4
whitemyer-advertising <webdev1@whitemyer.com>
2019-04-26 18:19:57 +0000

    New build

---
+++ build/precache-manifest.a2a9ed3689965b93be84b8b9c5058f48.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = [
+  {
+    "revision": "31dd83fc82e11c9f745ea5bfcd5d8053",
+    "url": "/beech-testing/static/media/beech-difference-compass.31dd83fc.png"
+  },
+  {
+    "revision": "7562ee8168fa72a5344e",
+    "url": "/beech-testing/static/css/main.5c87020c.chunk.css"
+  },
+  {
+    "revision": "e5ede8aa6877a7b2b3ca",
+    "url": "/beech-testing/static/js/runtime~main.e5ede8aa.js"
+  },
+  {
+    "revision": "69310104297b2bdbe73e101c28ecf20a",
+    "url": "/beech-testing/static/media/platform-foot-pedal-pump.69310104.png"
+  },
+  {
+    "revision": "f3a4b869baf9ecc13295",
+    "url": "/beech-testing/static/js/2.f3a4b869.chunk.js"
+  },
+  {
+    "revision": "7679d2be1f892063f679a8bed067839f",
+    "url": "/beech-testing/static/media/select-right-model.7679d2be.jpg"
+  },
+  {
+    "revision": "7a0318f247d7f5857150880c8ebc2f03",
+    "url": "/beech-testing/static/media/header.7a0318f2.jpg"
+  },
+  {
+    "revision": "cca2a6ee4b8f9099aa4c84d5bffaf28a",
+    "url": "/beech-testing/static/media/header.cca2a6ee.jpg"
+  },
+  {
+    "revision": "fadd6ca2b78cd3080fdc9fd6bcdcb2c1",
+    "url": "/beech-testing/static/media/header.fadd6ca2.jpg"
+  },
+  {
+    "revision": "0fc7950efb59498d245562c20b447b6f",
+    "url": "/beech-testing/static/media/header.0fc7950e.jpg"
+  },
+  {
+    "revision": "088a3bb190918cb0c289113ae59a74a3",
+    "url": "/beech-testing/static/media/header.088a3bb1.jpg"
+  },
+  {
+    "revision": "62e260b58e7c00724f2bc10065954bac",
+    "url": "/beech-testing/static/media/img-1.62e260b5.png"
+  },
+  {
+    "revision": "e19f82d1f90bdb84a6439bdd5b7e0066",
+    "url": "/beech-testing/static/media/category-intro.e19f82d1.jpg"
+  },
+  {
+    "revision": "53fd72634f12f610dcda7ea11695b5af",
+    "url": "/beech-testing/static/media/beech-difference.53fd7263.png"
+  },
+  {
+    "revision": "a7ca59f0d4a43c8480bd66680806749a",
+    "url": "/beech-testing/static/media/platform-stackers.a7ca59f0.jpg"
+  },
+  {
+    "revision": "8bf498afbd95e404da9295072434eacd",
+    "url": "/beech-testing/static/media/fork-stackers.8bf498af.jpg"
+  },
+  {
+    "revision": "789d96e6d84608303c9b73783e899a04",
+    "url": "/beech-testing/static/media/counterweighted-cranes.789d96e6.jpg"
+  },
+  {
+    "revision": "fef75f514dc1bd59f0f7ca8cfd4274e7",
+    "url": "/beech-testing/static/media/counterweighted-winch.fef75f51.png"
+  },
+  {
+    "revision": "a7cbc1ceeed0ee1a63e7677dfc6980c2",
+    "url": "/beech-testing/static/media/custom.a7cbc1ce.png"
+  },
+  {
+    "revision": "8663795c933e5bd16437a3ed6e9e9d8f",
+    "url": "/beech-testing/static/media/counterweighted-power-lift.8663795c.png"
+  },
+  {
+    "revision": "e9d6aaf55e8aa39e96bcdc774f727a5e",
+    "url": "/beech-testing/static/media/custom.e9d6aaf5.png"
+  },
+  {
+    "revision": "7562ee8168fa72a5344e",
+    "url": "/beech-testing/static/js/main.7562ee81.chunk.js"
+  },
+  {
+    "revision": "fca3f6297e361109563cf24e98cc905f",
+    "url": "/beech-testing/static/media/documentation.fca3f629.png"
+  },
+  {
+    "revision": "6bc61aea03c5e24151c918a755053844",
+    "url": "/beech-testing/static/media/platform-foot-pedal-pump-specs-drawing.6bc61aea.png"
+  },
+  {
+    "revision": "06c497f99f721c56db159b395c5b71bf",
+    "url": "/beech-testing/static/media/phone-icon.06c497f9.png"
+  },
+  {
+    "revision": "2bc57b13111ea51bf3c5bc5d334cda21",
+    "url": "/beech-testing/static/media/category-2.2bc57b13.png"
+  },
+  {
+    "revision": "f0646b85a971b6b12e52a679086fe401",
+    "url": "/beech-testing/static/media/platform-stacker.f0646b85.png"
+  },
+  {
+    "revision": "988e815bc86dff2a6b5fa657d7bcf2fe",
+    "url": "/beech-testing/static/media/custom-built.988e815b.png"
+  },
+  {
+    "revision": "6d3873bced834d3f3f7471e89584fd71",
+    "url": "/beech-testing/static/media/custom-bg.6d3873bc.jpg"
+  },
+  {
+    "revision": "f2119e687932ae7b0e6e4f8245cbeb6f",
+    "url": "/beech-testing/static/media/green-bg-with-logo.f2119e68.jpg"
+  },
+  {
+    "revision": "eb46c3417ca95e13030fc07bd8154a40",
+    "url": "/beech-testing/static/media/header-overlay.eb46c341.png"
+  },
+  {
+    "revision": "8d40a0fa90adf7e695953aec9473b363",
+    "url": "/beech-testing/static/media/product-types-bg.8d40a0fa.jpg"
+  },
+  {
+    "revision": "97dbc47fdaee1678f8cbce18a36fffb1",
+    "url": "/beech-testing/static/media/counterweighted-winch-stackers.97dbc47f.jpg"
+  },
+  {
+    "revision": "6404f59b9e657d407ddd87eb51e35351",
+    "url": "/beech-testing/static/media/pallet-stackers.6404f59b.jpg"
+  },
+  {
+    "revision": "345424533ffb33134f27eb0b1ca21bea",
+    "url": "/beech-testing/static/media/category-intro.34542453.jpg"
+  },
+  {
+    "revision": "ea36d345f36cbe831a6786233389b69a",
+    "url": "/beech-testing/static/media/straddle-cranes.ea36d345.jpg"
+  },
+  {
+    "revision": "d923e2c698ae99328757f6ea47b6878d",
+    "url": "/beech-testing/static/media/category-intro.d923e2c6.jpg"
+  },
+  {
+    "revision": "97cf6c4af3512de33a263f5195d217f8",
+    "url": "/beech-testing/static/media/loadredi-scissor-lift-tables.97cf6c4a.jpg"
+  },
+  {
+    "revision": "f96a1d5b732b7992c2dcc343fe743cdb",
+    "url": "/beech-testing/static/media/loadredi-tilt-lift-tables.f96a1d5b.jpg"
+  },
+  {
+    "revision": "bd9d52fcfad8a4ca4c8402b9dc109a10",
+    "url": "/beech-testing/static/media/category-intro.bd9d52fc.jpg"
+  },
+  {
+    "revision": "f3a4b869baf9ecc13295",
+    "url": "/beech-testing/static/css/2.acdfd482.chunk.css"
+  },
+  {
+    "revision": "dbca5fd7dfc94bd966b52aeccb58d10a",
+    "url": "/beech-testing/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.665c788e656432c912b327ddc5cdad12.js"
+  "/beech-testing/precache-manifest.a2a9ed3689965b93be84b8b9c5058f48.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/beech-testing/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
