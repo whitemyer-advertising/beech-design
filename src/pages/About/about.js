77c524563c243d21d7109a3806941fbc8545f570
whitemyer-advertising <webdev1@whitemyer.com>
2019-04-26 18:16:55 +0000

    About page layout and style

--- src/pages/About/about.js
+++ src/pages/About/about.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 import { AboutStyles } from "./styles";
+import { Theme } from '../../components/Styles/theme';
 
 export const About = () => (
     <AboutStyles>
@@ -8,8 +9,13 @@ export const About = () => (
             <title>Premium Material Handling Equipment | Beech Design & Manufacturing</title>
             <meta name="description" content="Beech Design & Manufacturing provides a full line of stackers for your material handling needs, including custom designed equipment.  All are factory proof-tested to 150% of capacity and come with a 2-year warranty on machine and 5-year warranty on weldment." />
         </Helmet>
-        <p>Beech Design & Manufacturing was formed in 1996 as a division of Miller Studio, Incorporated. MSI has 75 years of experience in manufacturing and distribution. The Beech Design & Manufacturing Division designs and manufactures premium material handling equipment. Both standard models and custom built units, designed to solve special problems, are available.</p>
-        <p>Our focus is on the industrial / professional market; therefore, our equipment must meet exceptionally high standards for quality and safety. We do this by exceeding the ASME and ANSI design and testing standards set for the various types of machines we build.</p>
-        <p>We market our equipment only through material handling and industrial supply distributors. Call for pricing and availability.</p>
+        <div id="wrapper">
+        <h1>About Us</h1>
+        <Theme.Rule />
+        <p>Beech Design & Manufacturing is a division of Miller Studio, Incorporated. Founded in 1934, MSI was known for its proud tradition of designing and building all the special equipment they needed.   And in 1996, when an employee had an idea to produce high-quality material handling equipment at fair prices and offer next-day delivery, Beech Design & Manufacturing was formed.
+<br /><br />Today Beech Design & Manufacturing designs and manufactures premium material handling equipment including stackers, floor cranes, scissor lift tables and vertical reciprocating conveyors.  Both standard models and custom built units, designed to solve special problems, are available.
+<br /><br />Our focus is on the industrial / professional market; therefore, our equipment must meet exceptionally high standards for quality and safety. We do this by exceeding the ASME and ANSI design and testing standards set for the various types of machines we build.
+</p>
+        </div>
     </AboutStyles>
 );
--- src/pages/About/styles.js
+++ src/pages/About/styles.js
@@ -8,4 +8,32 @@ export const AboutStyles = styled.div`
     }
     @media (max-width: 768px) {
     }
+    height: calc(100vh - 390px);
+    min-height: fit-content;
+    padding: 20px;
+    background-color: #1C4220;
+
+    h1 {
+        color: white;
+    }
+
+    hr {
+        max-width: 1000px;
+    }
+
+    #wrapper{
+        max-width: 1000px;
+        top: 50%;
+        -ms-transform: translateY(-50%);
+        transform: translateY(-50%);
+        position: relative;
+        margin: 0 auto;
+    }
+
+    p {
+        border-radius: 3px;
+        box-shadow: 0px 7px 7px #333;
+        background-color: #E5DCCA;
+        padding: 10px;
+    }
 `;
